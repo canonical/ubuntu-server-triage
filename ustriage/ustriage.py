d04b331bb673842c34f89b06ebc831f97621178e
Joshua Powers <josh.powers@canonical.com>
2017-06-22 10:50:39 -0700

    Creating task file
    Moving the launchpadlib task work to it's own file and additional
    cleanup of the ustriage file.

--- ustriage/ustriage.py
+++ ustriage/ustriage.py
@@ -3,18 +3,18 @@
 Output Ubuntu Server Launchpad bugs that for triage. Script accepts either
 a single date or inclusive range to find bugs.
 
-Copyright 2016 Canonical Ltd.
+Copyright 2017 Canonical Ltd.
 Joshua Powers <josh.powers@canonical.com>
 """
 import argparse
 from datetime import datetime, timedelta
-from functools import lru_cache
 import logging
 import sys
 import webbrowser
 
 from launchpadlib.launchpad import Launchpad
 
+from task import Task
 
 PACKAGE_BLACKLIST = {
     'cloud-init',
@@ -26,114 +26,6 @@ PACKAGE_BLACKLIST = {
     'maas',
 }
 TEAMLPNAME = "ubuntu-server"
-
-
-class Task(object):
-    """
-    Our representation of a Launchpad task.
-
-    This encapsulates a launchpadlib Task object, caches some queries,
-    stores some other properties (eg. the team-"subscribed"-ness) as needed
-    by callers, and presents a bunch of derived properties. All Task property
-    specific handling is encapsulated here.
-    """
-    LONG_URL_ROOT = 'pad.lv/'
-    SHORTLINK_ROOT = 'LP: #'
-    BUG_NUMBER_LENGTH = 7
-
-    def __init__(self):
-        # Whether the team is subscribed to the bug
-        self.subscribed = None
-        # Whether the last activity was by us
-        self.last_activity_ours = None
-        self.obj = None
-
-    @staticmethod
-    def create_from_launchpadlib_object(obj, **kwargs):
-        """Create object from launchpadlib"""
-        self = Task()
-        self.obj = obj
-        for key, value in kwargs.items():
-            setattr(self, key, value)
-        return self
-
-    @property
-    def url(self):
-        """The user-facing URL of the task"""
-        return self.LONG_URL_ROOT + self.number
-
-    @property
-    def shortlink(self):
-        """The user-facing "shortlink" that gnome-terminal will autolink"""
-        return self.SHORTLINK_ROOT + self.number
-
-    @property
-    @lru_cache()
-    def number(self):
-        """The bug number as a string"""
-        # This could be str(self.obj.bug.id) but using self.title is
-        # significantly faster
-        return self.title.split(' ')[1].replace('#', '')
-
-    @property
-    @lru_cache()
-    def src(self):
-        # This could be self.target.name but using self.title is
-        # significantly faster
-        """The source package name"""
-        return self.title.split(' ')[3]
-
-    @property
-    @lru_cache()
-    def title(self):
-        """The "title" as returned by launchpadlib"""
-        return self.obj.title
-
-    @property
-    @lru_cache()
-    def status(self):
-        """The "status" as returned by launchpadlib"""
-        return self.obj.status
-
-    @property
-    @lru_cache()
-    def short_title(self):
-        """Just the bug summary"""
-        # This could be self.obj.bug.title but using self.title is
-        # significantly faster
-        return ' '.join(self.title.split(' ')[5:]).replace('"', '')
-
-    def compose_pretty(self, shortlinks=True):
-        """Compose a printable line of relevant information"""
-        if shortlinks:
-            format_string = (
-                '%-' +
-                str(self.BUG_NUMBER_LENGTH + len(self.SHORTLINK_ROOT)) +
-                's'
-            )
-            bug_url = format_string % self.shortlink
-        else:
-            format_string = (
-                '%-' +
-                str(self.BUG_NUMBER_LENGTH + len(self.LONG_URL_ROOT)) +
-                's'
-            )
-            bug_url = format_string % self.url
-
-        flags = '%s%s' % (
-            '*' if self.subscribed else '',
-            '†' if self.last_activity_ours else '',
-        )
-
-        return '%s - %-16s %-16s - %s' % (
-            bug_url,
-            ('%s(%s)' % (flags, self.status)),
-            ('[%s]' % self.src), self.short_title
-        )
-
-    def sort_key(self):
-        """Sort method"""
-        return (not self.last_activity_ours, self.src)
 
 
 def connect_launchpad():
@@ -246,8 +138,8 @@ def last_activity_ours(task, activitysubscribers):
     )
 
 
-def modified_bugs(start_date, end_date, lpname, bugsubscriber,
-                  activitysubscribers, tag):
+def create_bug_list(start_date, end_date, lpname, bugsubscriber,
+                    activitysubscribers, tag=None):
     """
     Returns a list of bugs modified between dates.
     """
@@ -304,20 +196,6 @@ def modified_bugs(start_date, end_date, lpname, bugsubscriber,
     }
 
     return bugs
-
-
-def create_bug_list(start_date, end_date, lpname, bugsubscriber,
-                    activitysubscribers, tag=None):
-    """
-    Subtracts all bugs modified after specified start and end dates.
-
-    This provides the list of bugs between two dates as Launchpad does
-    not appear to have a specific function for searching for a range.
-    """
-    tasks = modified_bugs(start_date, end_date, lpname, bugsubscriber,
-                          activitysubscribers, tag)
-
-    return tasks
 
 
 def report_current_backlog(lpname):
