37ff9d260e3958dbb5cbbbcf78c7e485a16113c7
Joshua Powers <josh.powers@canonical.com>
2017-06-27 12:35:39 -0700

    Adding key to symbols in output. Thanks andreas!

--- ustriage/ustriage.py
+++ ustriage/ustriage.py
@@ -283,6 +283,8 @@ def main(date_range=None, debug=False, open_browser=None,
     # Need to make date range inclusive
     end = datetime.strptime(date_range['end'], '%Y-%m-%d') - timedelta(days=1)
     end = end.strftime('%Y-%m-%d')
+    logging.info('\'*\': %s is directly subscribed', lpname)
+    logging.info('\'†\': last bug activity is ours')
     logging.info('Bugs for triage on %s to %s (inclusive)',
                  date_range['start'], end)
 
