1a0c9501749dc8219bc37041e18c4aed54a1deed
Joshua Powers <josh.powers@canonical.com>
2018-03-30 15:07:00 -0700

    remove unicode

--- ustriage/ustriage.py
+++ ustriage/ustriage.py
@@ -294,7 +294,7 @@ def main(date_range=None, debug=False, open_browser=None,
     end = datetime.strptime(date_range['end'], '%Y-%m-%d') - timedelta(days=1)
     end = end.strftime('%Y-%m-%d')
     logging.info('\'*\': %s is directly subscribed', lpname)
-    logging.info(u'\'†\': last bug activity is ours')
+    logging.info('\'+\': last bug activity is ours')
     logging.info('Bugs for triage on %s to %s (inclusive)',
                  date_range['start'], end)
 
